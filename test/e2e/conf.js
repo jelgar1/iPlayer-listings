7d4ceaa480cc8c52e73f00f8f250747b8585494f
Jelgar <jonathanelgardiner@gmail.com>
2016-04-01 12:26:53 +0100

    feature test for pages

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,4 +1,9 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['iPlayerListingsFeature.js']
+  specs: ['iPlayerListingsFeature.js'],
+  multiCapabilities: [{
+    browserName: 'firefox'
+  }, {
+    browserName: 'chrome'
+  }]
 };
--- test/e2e/iPlayerListingsFeature.js
+++ test/e2e/iPlayerListingsFeature.js
@@ -1,10 +1,21 @@
 describe('iPlayer Listings finder', function() {
-  it('finds listings', function() {
+  beforeEach(function() {
     browser.get('http://localhost:8080');
-
-    element(by.className('btn')).click();
-
-    expect(element(by.binding('show.title')).getText()).
-        toEqual('Abadas');
+    listings = element.all(by.repeater('show in ctrl.searchResult.atoz_programmes.elements'));
+    pages = element.all(by.repeater('page in ctrl.pages'));
   });
+
+  it('has a title', function() {
+    expect(browser.getTitle()).toEqual('iPlayer Listings');
+  });
+
+  it('finds listings', function() {
+    element(by.className('btn')).click();
+    expect(listings.first().getText()).toEqual('Abadas');
+  });
+
+  it('finds the number of pages', function() {
+    expect(listings.last().getText()).toEqual('4');
+  });
+
 });
